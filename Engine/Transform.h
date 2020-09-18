d8dc0e9ceb0893e2063eb6bca831f47d26fc24e8
Nikita Chernyakov <207958@niuitmo.ru>
2020-09-18 17:09:20 +0300

    Refactor Transform component
    - Fix formating and naming
    - Add missing functions and fix some problems with removing parent

--- Engine/Transform.h
+++ Engine/Transform.h
@@ -1,40 +1,49 @@
 #pragma once
 
-#include <d3d11.h>
 #include "SimpleMath.h"
 
 using namespace DirectX::SimpleMath;
 
-struct Transform
+class Transform
 {
+
 public:
-	Transform() = default;
-	Transform(Vector3 pos);
-	~Transform() = default;
 
-	Transform& getParent() const;
-	std::vector<std::shared_ptr<Transform>>& getChild();
-	void addChild(Transform* obj);
-	void setParent(Transform* parent);
+	Transform(Vector3 pos = Vector3::Zero);
 
-	Vector3 getLocalPosition() const;
-	void setLocalPosition(Vector3 pos);
-	void addLocalPosition(Vector3 pos);
+	~Transform();
+
+	Transform* GetParent() const;
+	bool HasParent() const;
+	void SetParent(Transform* pParent);
+	void ClearParent(bool recursiveClearing = true);
+
+	std::vector<Transform*>& GetChildren();
+	void AddChild(Transform* pChild);
+	void RemoveChild(Transform* pChildToRemove, bool recursiveClearing = true);
+
+	Vector3 GetLocalPosition() const;
+	void SetLocalPosition(Vector3 pos);
+	void AddLocalPosition(Vector3 pos);
 	
-	Vector3 getWorldPosition() const;
-	void setWorldPosition(Vector3 pos);
-	void addWorldPosition(Vector3 pos);
+	Vector3 GetWorldPosition() const;
+	void SetWorldPosition(Vector3 pos);
+	void AddWorldPosition(Vector3 pos);
 
-	std::unique_ptr<Transform> parent = nullptr;
-	std::vector<std::shared_ptr<Transform>> children;
+	void AddLocalRotation(Vector3 axis, float angle);
+	void AddWorldRotation(Vector3 axis, float angle);
 
-	void addLocalRotation(Vector3 axis, float angle);
-	void addWorldRotation(Vector3 axis, float angle);
+	Matrix GetWorldMatrix() const;
 
-	Matrix getWorldMatrix() const;
-	Matrix m_transformMatrix = Matrix::Identity;
-	Matrix m_Translation = Matrix::Identity;
-	Matrix m_Rotation = Matrix::Identity;
-	Matrix m_Scale = Matrix::Identity;
+private:
+
+	Matrix transformMatrix = Matrix::Identity;
+	Matrix translation = Matrix::Identity;
+	Matrix rotation = Matrix::Identity;
+	Matrix scale = Matrix::Identity;
+
+	Transform* pParent = nullptr;
+	std::vector<Transform*> children;
+
 };
 

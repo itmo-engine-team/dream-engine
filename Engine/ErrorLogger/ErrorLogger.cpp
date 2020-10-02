5736477e1cafdbb6d8281626ad31686c40e47c50
Kirill Karpov <37740237+Jass2100@users.noreply.github.com>
2020-10-02 21:01:32 +0300

    Change logger format (#15)
    * Change logger format
    
    * Added Info in logger

--- Engine/ErrorLogger/ErrorLogger.cpp
+++ Engine/ErrorLogger/ErrorLogger.cpp
@@ -11,10 +11,13 @@ void ErrorLogger::Log(LogType type, std::string message)
 	switch (type)
 	{
 	case Error:
-		errorMessage += "\nError: ";
+		errorMessage += "\nERROR: ";
 		break;
 	case Warning:
-		errorMessage += "\nWarning: ";
+		errorMessage += "\nWARNING: ";
+		break;
+	case Info:
+		errorMessage += "\nINFO: ";
 		break;
 	}	
 	errorMessage += message;
@@ -25,7 +28,7 @@ void ErrorLogger::Log(LogType type, std::string message)
 
 	if (logFile.is_open() == true)
 	{
-		logFile << "\n" << errorMessage << std::endl;
+		logFile << "\n" << errorMessage << "\n" << std::endl;
 		logFile.close();
 	}
 	printf(errorMessage.c_str());
@@ -46,21 +49,23 @@ void ErrorLogger::DirectXLog(HRESULT hr, LogType type, const std::string& msg, c
 	switch (type)
 	{
 	case Warning:
-		errorMessage += "Warning: ";
+		errorMessage += "\nWARNING: ";
 		break;
 	case Error:
-		errorMessage += "Error: ";
+		errorMessage += "\nERROR: ";
+		break;
+	case Info:
+		errorMessage += "\nINFO: ";
 		break;
 	}
 		
 	errorMessage += std::string(msg);
 	errorMessage += "\nFile: " + file;
-	errorMessage += "\nFunction: " + function;
-	errorMessage += "\nLine: " + std::to_string(line);
+	errorMessage += "\nFunction: " + function + " [" + std::to_string(line) + "] ";
 
 	if (logFile.is_open() == true)
 	{
-		logFile << "\n" << errorMessage << std::endl;
+		logFile << "\n" << errorMessage << "\n" <<std::endl;
 		logFile.close();
 	}
 	
@@ -70,8 +75,8 @@ void ErrorLogger::DirectXLog(HRESULT hr, LogType type, const std::string& msg, c
 		printf(errorMessage.c_str());
 		//MessageBoxA(GetActiveWindow(), error_message.c_str(), "Error", MB_ICONERROR);
 		exit(0);
-	case Warning:
-		break;
+	default:
+		printf(errorMessage.c_str());
 	}
 	
 	printf(errorMessage.c_str());

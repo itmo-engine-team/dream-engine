299353162cf879c27d832bfa63d8b0d780ebfc4c
Nikita Chernyakov <207958@niuitmo.ru>
2020-10-05 22:38:23 +0300

    Fix transform calculations (#17)

--- Engine/InGame/GameComponent/Transform.h
+++ Engine/InGame/GameComponent/Transform.h
@@ -10,37 +10,38 @@ class Transform
 public:
 
     Transform(Vector3 pos = Vector3::Zero);
-
     ~Transform();
 
     Transform* GetParent() const;
     bool HasParent() const;
-    void SetParent(Transform* parent);
+    void SetParent(Transform* parent, bool saveRelation = false);
     void ClearParent();
 
     std::vector<Transform*>& GetChildren();
     void AddChild(Transform* child);
     void RemoveChild(Transform* childToRemove);
 
-    Vector3 GetLocalPosition() const;
     void SetLocalPosition(Vector3 pos);
     void AddLocalPosition(Vector3 pos);
+    void AddLocalRotation(Vector3 axis, float angle);
+
+    // Relates on parent
+    Vector3 GetRelativePosition() const;
+    void SetRelativePosition(Vector3 pos);
+    void AddRelativePosition(Vector3 pos);
+    void AddRelativeRotation(Vector3 axis, float angle);
 
     Vector3 GetWorldPosition() const;
     void SetWorldPosition(Vector3 pos);
     void AddWorldPosition(Vector3 pos);
-
-    void AddLocalRotation(Vector3 axis, float angle);
     void AddWorldRotation(Vector3 axis, float angle);
 
     Matrix GetWorldMatrix() const;
 
 private:
 
-    Matrix transformMatrix = Matrix::Identity;
-    Matrix translation = Matrix::Identity;
-    Matrix rotation = Matrix::Identity;
-    Matrix scale = Matrix::Identity;
+    // Transform matrix from local to parent
+    Matrix relativeMatrix = Matrix::Identity;
 
     Transform* parent = nullptr;
     std::vector<Transform*> children;

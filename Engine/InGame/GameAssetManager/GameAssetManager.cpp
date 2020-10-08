ee8bc81c9621c6257168bf4244dc7a56316c74ae
Nikita Chernyakov <207958@niuitmo.ru>
2020-10-08 20:26:39 +0300

    Implement simple GameAssetManager (#20)

---
+++ Engine/InGame/GameAssetManager/GameAssetManager.cpp
@@ -0,0 +1,47 @@
+#include "GameAssetManager.h"
+
+GameAssetManager::GameAssetManager(Engine* engine) : engine(engine)
+{
+
+}
+
+GameAssetManager::~GameAssetManager()
+{
+    // TODO 
+}
+
+void GameAssetManager::AddGameObject(GameObject* gameObject)
+{
+    gameObjects.push_back(gameObject);
+}
+
+std::vector<GameObject*> GameAssetManager::GetGameObjects() const
+{
+    return gameObjects;
+}
+
+void GameAssetManager::AddActor(Actor* actor)
+{
+    actors.push_back(actor);
+    AddGameObject(actor);
+}
+
+std::vector<Actor*> GameAssetManager::GetActors() const
+{
+    return actors;
+}
+
+void GameAssetManager::AddShader(Shader* shader)
+{
+    shaders.push_back(shader);
+}
+
+void GameAssetManager::AddTexture(Texture* texture)
+{
+    textures.push_back(texture);
+}
+
+void GameAssetManager::AddModel(ModelData* modelData)
+{
+    models.push_back(modelData);
+}

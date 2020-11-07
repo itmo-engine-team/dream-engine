cc951b1225f4e43549eb15eec4fed82aec29aba7
Kirill <jass1997@yandex.ru>
2020-11-07 21:38:31 +0300

    Deleted Test class from JsonTest

--- UnitTest/JsonTest.cpp
+++ UnitTest/JsonTest.cpp
@@ -1,105 +1,18 @@
 #include "pch.h"
 
-#include "nlohmann/json.hpp"
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <filesystem>
+#include "AssetServices.h"
 
-class JsonTest : public ::testing::Test
-{
-    using json = nlohmann::json;
-
-public:
-
-   void JsonTest::TestJsRead()
-   {
-        std::ifstream file("Person.json");
-        json j;
-
-        file >> j;
-
-        std::string objectName;
-        std::string name;
-        int age = 0;
-
-        if (j.find("Object name") != j.end())
-        {
-            objectName = j["Object name"];
-        }
-
-        if (j.find("Name") != j.end())
-        {
-            name = j["Name"];
-        }
-
-        if (j.find("Age") != j.end())
-        {
-            age = j["Age"].get<int>();
-        }
-
-        std::cout << j << "\n";
-        std::cout << objectName << "\n";
-        std::cout << name << "\n";
-        std::cout << age;
-   }
-
-   void JsonTest::TestJsCreate(std::string fileRelativePath, std::string name, int age)
-   {
-        std::string pathToFile = fileRelativePath;
-        json j;
-        std::filesystem::path p(pathToFile);
-
-        if (!std::filesystem::exists(pathToFile))
-        {
-            create_directories(p.parent_path());
-        }
-        std::cout << p.extension().string();
-        j["Object name"] = p.stem().string();
-        j["Name"] = name;
-        j["Age"] = age;
-       
-        std::ofstream file(pathToFile);
-        file << std::setw(4) << j << std::endl;
-   }
-
-   void JsonTest::FindAssets()
-   {
-        std::string directory_name = "Content";
-        std::string extension = ".asset";
-
-        try // Exception for directories not found
-        {
-            for (auto& p : std::filesystem::recursive_directory_iterator(directory_name)) //For all files in folders
-            {
-                if (p.path().extension() != extension)
-                    continue;
-
-                std::ifstream file(p.path());
-                json j;
-                file >> j;
-                
-                std::cout << j["Name"];
-            }
-        }
-        catch (std::exception& e)
-        {
-            std::cout << "Error: " << e.what() << '\n';
-        }
-   }
-};
-
-TEST_F(JsonTest, CreateTest)
+TEST(JsonTest, CreateTest)
 {   
-    JsonTest::TestJsCreate("Content/Person1.asset", "Alex", 25);
+    AssetServices::CreateAsset("Content/Person1.asset");
 }
 
-TEST_F(JsonTest, ReadTest)
+TEST(JsonTest, ReadTest)
 {    
-    JsonTest::TestJsRead();
+   // AssetServices::AssetReader
 }
 
-TEST_F(JsonTest, FindTest)
+TEST(JsonTest, FindTest)
 {
-    FindAssets();
+    AssetServices::FindAssets();
 }

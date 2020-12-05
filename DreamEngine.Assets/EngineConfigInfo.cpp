54ea8594cefdcb07ee6ef486dac0afc95ee0cc19
Nikita Chernyakov <207958@niuitmo.ru>
2020-12-05 19:05:42 +0300

    Integrate editor config in engine

--- DreamEngine.Assets/EngineConfigInfo.cpp
+++ DreamEngine.Assets/EngineConfigInfo.cpp
@@ -1,8 +1,21 @@
 #include "EngineConfigInfo.h"
 
+Json EngineConfigInfo::ToJson()
+{
+    Json json = Serializable::ToJson();
+
+    json["isGameMode"] = isGameMode;
+    json["screenWidth"] = screenWidth;
+    json["screenHeight"] = screenHeight;
+
+    return json;
+}
+
 void EngineConfigInfo::FromJson(Json json)
 {
     isGameMode = json["isGameMode"].get<bool>();
+    screenWidth = json["screenWidth"].get<int>();
+    screenHeight = json["screenHeight"].get<int>();
 }
 
 bool EngineConfigInfo::IsGameMode()
@@ -10,9 +23,12 @@ bool EngineConfigInfo::IsGameMode()
     return isGameMode;
 }
 
-Json EngineConfigInfo::ToJson()
+int EngineConfigInfo::GetScreenWidth() const
 {
-    Json json = Serializable::ToJson();
-    json["isGameMode"] = isGameMode;
-    return json;
+    return screenWidth;
+}
+
+int EngineConfigInfo::GetScreenHeight() const
+{
+    return screenHeight;
 }

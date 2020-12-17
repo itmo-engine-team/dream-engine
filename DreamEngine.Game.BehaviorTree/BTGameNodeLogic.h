ed5e3fbe8be37e7b26e855804caa5537d039e4a2
Kirill <jass1997@yandex.ru>
2020-12-17 21:35:49 +0300

    Moved test variable

--- DreamEngine.Game.BehaviorTree/BTGameNodeLogic.h
+++ DreamEngine.Game.BehaviorTree/BTGameNodeLogic.h
@@ -7,12 +7,12 @@ class BTGameNodeLogic : public BTGameNode
 
 public:
 
+    //TODO: delete after all tests end
+    bool TestCheckResult = false;
+
     BTGameNodeLogic() = delete;
     BTGameNodeLogic(BTGameNodeRoot* parentNode);
     BTGameNodeLogic(BTGameNodeComposite* parentNode);
-
-    //TODO: delete after all tests end
-    bool TestCheckResult = false;
 
     bool Run() override;
 

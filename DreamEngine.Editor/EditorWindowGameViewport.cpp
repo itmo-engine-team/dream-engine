86e0b9501823e4c98b0c023362baffbaa00f34d2
MMTsallagova <55704942+MMTsallagova@users.noreply.github.com>
2020-12-23 20:41:25 +0300

    Add Scene Hierarchy, Room and actor context menu, texture size (#72)
    * Add Scene Hierarchy, Room and actor context menu, texture size
    
    * Fix menu bar for game viewport window
    
    * Move class funtions to GameViewport, fix viewport size
    
    * Add renderGameViewport() and renderSceneHierarchy()
    
    * Rename variable
    
    * delete MenuBar
    
    Co-authored-by: Nikita Chernyakov <207958@niuitmo.ru>

--- DreamEngine.Editor/EditorWindowGameViewport.cpp
+++ DreamEngine.Editor/EditorWindowGameViewport.cpp
@@ -4,10 +4,13 @@
 #include "Editor.h"
 #include "Graphics.h"
 
+#include "AssetManager.h"
+
 EditorWindowGameViewport::EditorWindowGameViewport(Editor* editor)
     : EditorWindow("Game Viewport", editor)
 {
-
+    assetTree = editor->GetAssetManager()->GetContentAssetTree();
+    currentScene = assetTree->GetRootNode();
 }
 
 void EditorWindowGameViewport::Update()
@@ -17,7 +20,144 @@ void EditorWindowGameViewport::Update()
 
 void EditorWindowGameViewport::Render()
 {
+    renderGameViewport();
+    renderSceneHierarchy();
+}
+
+void EditorWindowGameViewport::updateViewportSize()
+{
+    ImVec2 windowSize = ImGui::GetWindowSize();
+    gameViewportSizeMultiplier = min(windowSize.x / GAME_VIEWPORT_RATIO.x, windowSize.y / GAME_VIEWPORT_RATIO.y);
+    gameViewportSize = ImVec2(GAME_VIEWPORT_RATIO.x * gameViewportSizeMultiplier, GAME_VIEWPORT_RATIO.y * gameViewportSizeMultiplier);
+}
+
+void EditorWindowGameViewport::renderGameViewport()
+{
     ImGui::Begin("GameViewport");
-    ImGui::Image(editor->GetGraphics()->GetSceneResourceView(), ImVec2(400, 300));
+
+    if (ImGui::Button("Play")) 
+    { 
+        // TODO: Play game 
+    }
+
+    updateViewportSize();
+    ImGui::Image(editor->GetGraphics()->GetSceneResourceView(), gameViewportSize);
+
     ImGui::End();
+}
+
+void EditorWindowGameViewport::renderSceneHierarchy()
+{
+    ImGui::Begin("Scene Hierarchy");
+
+    if (ImGui::Button("Save"))
+    {
+        // TODO: add SaveScene
+    }
+
+    drawScenesTree(currentScene);
+
+    ImGui::End();
+}
+
+void EditorWindowGameViewport::drawScenesTree(FolderNode* sceneNode)
+{
+    bool treeExpanded = false;
+
+    auto flags = ImGuiTreeNodeFlags_OpenOnDoubleClick
+        | ImGuiTreeNodeFlags_FramePadding
+        | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+
+    if (currentScene == sceneNode)
+        flags |= ImGuiTreeNodeFlags_Selected;
+
+    if (ImGui::TreeNodeEx(sceneNode->GetName().c_str(), flags))
+    {
+        if (ImGui::IsItemClicked())
+        {
+            currentScene = sceneNode;
+        }
+
+        treeExpanded = true;
+
+        currentScene = sceneNode;
+
+        drawSceneContextMenu();
+
+        for (auto childFolderNode : sceneNode->GetChildFolderList())
+        {
+            //drawFolderTreeNode(childFolderNode, level + 1);
+        }
+    }
+
+    if (!treeExpanded)
+    {
+        if (ImGui::IsItemClicked())
+        {
+            currentScene = sceneNode;
+        }
+
+        drawSceneContextMenu();
+    }
+}
+
+void EditorWindowGameViewport::drawSceneContextMenu()
+{
+    if (ImGui::BeginPopupContextItem())
+    {
+        if (ImGui::Selectable("Edit"))
+        {
+            // TODO: add EditScene
+        }
+
+        if (ImGui::Selectable("Add prefab"))
+        {
+            // TODO: add AddPrefab
+        }
+
+        if (ImGui::Selectable("Add Actor"))
+        {
+            // TODO: add AddActor
+        }
+
+        if (ImGui::Selectable("Delete"))
+        {
+            // TODO: add DeleteScene
+        }
+
+        if (ImGui::Selectable("Rename"))
+        {
+            // TODO: add RenameScene
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void EditorWindowGameViewport::drawActorContextMenu()
+{
+    if (ImGui::BeginPopupContextItem())
+    {
+        if (ImGui::Selectable("Edit"))
+        {
+            // TODO: add EditActor
+        }
+
+        if (ImGui::Selectable("Save"))
+        {
+            // TODO: add SaveActor
+        }
+
+        if (ImGui::Selectable("Delete"))
+        {
+            // TODO: add DeleteActor
+        }
+
+        if (ImGui::Selectable("Rename"))
+        {
+            // TODO: add RenameActor
+        }
+
+        ImGui::EndPopup();
+    }
 }
--- DreamEngine.Editor/EditorWindowGameViewport.h
+++ DreamEngine.Editor/EditorWindowGameViewport.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "EditorWindow.h"
+#include "imgui.h"
+
+#include "AssetTree.h"
 
 class EditorWindowGameViewport : public EditorWindow
 {
@@ -12,5 +15,24 @@ public:
     void Update() override;
     void Render() override;
 
+
+private:
+
+    const ImVec2 GAME_VIEWPORT_RATIO = ImVec2(16, 9);
+    ImVec2 gameViewportSize;
+
+    float gameViewportSizeMultiplier = 20;
+
+    // for test 
+    AssetTree* assetTree;
+    FolderNode* currentScene = nullptr;
+
+    void updateViewportSize();
+    void renderGameViewport();
+    void renderSceneHierarchy();
+
+    void drawScenesTree(FolderNode* sceneNode);
+    void drawSceneContextMenu();
+    void drawActorContextMenu();
 };
 

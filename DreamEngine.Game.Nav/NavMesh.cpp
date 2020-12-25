59d9d871e15838e8f29aba8ea1d5b9d734615c98
Kirill Karpov <37740237+Jass2100@users.noreply.github.com>
2020-12-25 21:17:25 +0300

    Init Nav mesh grid (#75)
    * Added init Nav mesh grid
    
    * Fixed vertex name
    
    * Added path to Graphics project

--- DreamEngine.Game.Nav/NavMesh.cpp
+++ DreamEngine.Game.Nav/NavMesh.cpp
@@ -1,1 +1,99 @@
 #include "NavMesh.h"
+
+#include <iostream>
+
+NavMesh::NavMesh(Vector3 navMeshPosition, Vector3 planeSize, float polySize)
+{
+    size = Vector2(planeSize.x, planeSize.z);
+    if (planeSize.x > planeSize.z)
+        size.y = planeSize.x;
+    else
+        size.x = planeSize.z;
+
+    polygonSize = polySize;
+    position = navMeshPosition;
+
+    initNavMeshGrid();
+}
+
+Vector2 NavMesh::GetSize() const
+{
+    return size;
+}
+
+float NavMesh::GetPolygonSize() const
+{
+    return polygonSize;
+}
+
+void NavMesh::SetPolygonSize(float polySize)
+{
+    polygonSize = polySize;
+}
+
+Vector3 NavMesh::GetPosition() const
+{
+    return position;
+}
+
+std::vector<std::vector<NavMeshPolygon>> NavMesh::GetGrid() const
+{
+    return navMeshGrid;
+}
+
+void NavMesh::initNavMeshGrid()
+{
+    // Calculate center of coordinates of zero polygon
+    Vector3 centerFirstPolygon;
+    centerFirstPolygon.x = position.x + size.y / 2 - polygonSize / 2; //TODO: debugCheck
+    centerFirstPolygon.y = position.y;
+    centerFirstPolygon.z = position.z - size.x / 2 + polygonSize / 2; //TODO: debugCheck
+
+    for (int x = 0; x < size.x; x++)
+    {
+        for (int y = 0; y < size.y; y++)
+        {
+            std::vector <NavMeshPolygon> navMeshRow;
+
+            NavMeshPolygon polygon;      
+            polygon.center.x = polygonSize * y + centerFirstPolygon.x; //TODO: debugCheck
+            polygon.center.y = centerFirstPolygon.y;
+            polygon.center.z = polygonSize * x + centerFirstPolygon.z; //TODO: debugCheck
+
+            initVertex(polygon);
+            navMeshRow.push_back(polygon);
+
+            navMeshGrid.push_back(navMeshRow);
+        }
+    }
+}
+
+void NavMesh::initVertex(NavMeshPolygon& polygon) const
+{
+    float halfPolygonSize = polygonSize / 2;
+
+    polygon.VertexLT = new Vertex
+    {
+        Vector3{ polygon.center.x + halfPolygonSize, polygon.center.y, polygon.center.z - halfPolygonSize },
+        Vector4{ 0, 1, 0, 1 },
+        Vector3::Up
+    };
+    polygon.VertexLD = new Vertex
+    {
+        Vector3{ polygon.center.x - halfPolygonSize, polygon.center.y, polygon.center.z - halfPolygonSize },
+        Vector4{ 0, 1, 0, 1 },
+        Vector3::Up
+    };
+    polygon.VertexRT = new Vertex
+    {
+        Vector3{ polygon.center.x + halfPolygonSize, polygon.center.y, polygon.center.z + halfPolygonSize },
+        Vector4{ 0, 1, 0, 1 },
+        Vector3::Up
+    };
+    polygon.VertexRD = new Vertex
+    {
+        Vector3{ polygon.center.x - halfPolygonSize, polygon.center.y, polygon.center.z + halfPolygonSize },
+        Vector4{ 0, 1, 0, 1 },
+        Vector3::Up
+    };
+}
--- DreamEngine.Game.Nav/NavMesh.h
+++ DreamEngine.Game.Nav/NavMesh.h
@@ -1,10 +1,44 @@
 #pragma once
 
 #include "SimpleMath.h"
+#include "Vertex.h"
 
 using namespace DirectX::SimpleMath;
 
+struct NavMeshPolygon
+{
+    Vector3 center;
+    Vertex* VertexLT;
+    Vertex* VertexLD;
+    Vertex* VertexRT;
+    Vertex* VertexRD;
+};
+
 class NavMesh
 {
+
+public:
+    NavMesh() = delete;
+    NavMesh(Vector3 navMeshPosition, Vector3 planeSize, float polySize);
+
+    Vector2 GetSize() const;
+
+    float GetPolygonSize() const;
+    void SetPolygonSize(float polySize);
+
+    Vector3 GetPosition() const;
+
+    std::vector<std::vector<NavMeshPolygon>> GetGrid() const;
+
+protected:
+
+    Vector2 size;
+    Vector3 position;
+    float polygonSize;
+
+    std::vector<std::vector<NavMeshPolygon>> navMeshGrid;
+
+    void initNavMeshGrid();
+    void initVertex(NavMeshPolygon& polygon) const;
 };
 

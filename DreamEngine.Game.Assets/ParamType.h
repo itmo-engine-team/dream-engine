f63475d2e7c2e860d4dc05d3ddaeacf94041b3bc
Nikita Chernyakov <207958@niuitmo.ru>
2021-01-11 20:43:28 +0300

    Add string type for param, add map from param type to string

--- DreamEngine.Game.Assets/ParamType.h
+++ DreamEngine.Game.Assets/ParamType.h
@@ -5,5 +5,14 @@ enum class ParamType
     Int,
     Float,
     Asset,
-    Transform
+    Transform,
+    String
+};
+
+const std::map<ParamType, std::string> MAP_ASSET_TYPE_TO_STRING = {
+    { ParamType::Int, "Int" },
+    { ParamType::Float, "Float" },
+    { ParamType::Asset, "Asset" },
+    { ParamType::Transform, "Transform" },
+    { ParamType::String, "String" },
 };

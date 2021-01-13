1d6da84d09322dd9c3883edce88d73bd653a5a3b
Nikita Chernyakov <207958@niuitmo.ru>
2021-01-14 01:54:37 +0300

    Add component for game over on collect all target

--- DreamEngine.Game.Actor/ActorComponentFactory.h
+++ DreamEngine.Game.Actor/ActorComponentFactory.h
@@ -15,6 +15,7 @@
 #include "ACF_GameOverOnTargetCollision.h"
 #include "ACF_CollectScoreOnCollision.h"
 #include "ACF_Projectile.h"
+#include "ACF_GameOverOnCollectAllTarget.h"
 
 class ActorComponentFactory
 {
@@ -45,6 +46,7 @@ protected:
         { ACF_Type::Movement , new ACF_Creator_Movement() },
         { ACF_Type::PlayerMovement , new ACF_Creator_PlayerMovement() },
         { ACF_Type::GameOverOnTargetCollision, new ACF_Creator_GameOverOnTargetCollision() },
+        { ACF_Type::GameOverOnCollectAllTarget, new ACF_Creator_GameOverOnCollectAllTarget() },
         { ACF_Type::DestroyTargetOnCollision, new ACF_Creator_DestroyTargetOnCollision() },
         { ACF_Type::CollectScoreOnCollision, new ACF_Creator_CollectScoreOnCollision() },
         { ACF_Type::Projectile, new ACF_Creator_Projectile() },

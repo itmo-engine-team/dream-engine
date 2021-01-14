f027764802685c653b479c22e4d6c2859f480c5b
Maria Selivanova <37633030+tolliand@users.noreply.github.com>
2021-01-14 03:41:11 +0300

    finished maze

--- DreamEngine.Game/BaseSceneViewer.cpp
+++ DreamEngine.Game/BaseSceneViewer.cpp
@@ -163,7 +163,7 @@ void BaseSceneViewer::createBaseSceneActors()
     baseSceneActors.push_back(spectatorActor);
     actorContext->SetCamera(spectatorActor->GetCameraComponent());
 
-    planeModel = MeshRenderer::CreateBoxModel({ 1, 1, 1, 1 }, { 30, 0.1, 30 });
+    planeModel = MeshRenderer::CreateBoxModel({ 1, 1, 1, 1 }, { 20, 0.1, 20 });
     planeActor = new Actor(actorContext);
     planeActor->AddSceneComponent(new ACS_StaticModel(planeActor, planeModel));
     baseSceneActors.push_back(planeActor);
